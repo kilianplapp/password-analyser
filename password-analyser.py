18c04d730edf6a427b40b64fee9bd3bb8592b9d8
prithvidiamond1 <prithviraj645@gmail.com>
2020-10-07 20:51:55 +0530

    Fixed a major bug and made some improvements to the code

--- password-analyser.py
+++ password-analyser.py
@@ -5,4 +5,4 @@ import argparse
 parser = argparse.ArgumentParser(description="Check the strength of your password.")
 parser.add_argument('user_input', type=str, help=':Input a password')
 args = parser.parse_args()
-main.password_analyser(args)
+main.password_analyser(vars(args)['user_input'])
