ff889d0b61d7d3cb793ab69469f493708764b3e0
lucifer-999 <ayush.bajaj3210@gmail.com>
2020-10-08 22:09:36 +0530

    Added a option to load passwords from a file

--- password-analyser.py
+++ password-analyser.py
@@ -3,6 +3,20 @@ from src import main
 import argparse
 
 parser = argparse.ArgumentParser(description="Check the strength of your password.")
-parser.add_argument('user_input', type=str, help=':Input a password')
+parser.add_argument('user_input', type=str, help=':Input a password', nargs='?')
+parser.add_argument("-f", "--file", type = str, default = "", help = "Username list to report.")
+
 args = parser.parse_args()
-main.password_analyser(vars(args)['user_input'])
+
+if args.file == "" :
+    main.password_analyser(vars(args)['user_input'])
+
+else :
+    a = open(args.file, "r").readlines()
+
+    file = [s.rstrip()for s in a]
+    file.reverse()
+
+    for lines in file:
+        print("\nPassword: ", lines)
+        main.password_analyser(lines)
